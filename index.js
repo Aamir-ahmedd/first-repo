529e03757af81f1cd97e14168e75fd2b76a5a727
Amir <aamir8466@gmail.com>
2022-11-28 22:04:20 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+
+var sqd = "my name is amir";
+document.write(sqd);
+var job = "i m from military";
+document.write(job);
+var ex = "i m ex army";
+document.write(ex);
+var train = "two squad";
+document.write(train);
